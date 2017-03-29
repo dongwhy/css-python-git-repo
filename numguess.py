25f61892e407b58038fd964ac8fe30406221e1d3
dongwhy <kd9336w@naver.com>
2017-03-29 16:33:36 +0900

    add chance 3

--- numguess.py
+++ numguess.py
@@ -4,18 +4,22 @@ answer = random.randint(1,100)
 print(answer)
 
 username = input("What is your name? ")
+chance = 3
+
 while 1:
 	guess = eval(input("hi, " + username + ", GUess the number: "))
-
-	if guess == answer:
+	chance -= 1
+	
+	if chance == 0 :
+		print("You are dead. ")
+		break
+	elif guess == answer:
 		print("Correct!")
 		break
 	elif guess > answer :
-		print(str(guess) + " > answer")
+		print(str(guess) + " > answer, chance : " + str(chance))
 	elif guess < answer :
-		print(str(guess) + " < answer")
-	else :
-		print("You are wrong!")
+		print(str(guess) + " < answer, chance : " + str(chance))
 
 	
 	
